3957a2397210d57fb9099936df52eae1103fe4cd
Szymon <sgil73072@gmail.com>
2023-03-11 10:33:08 +0100

    change about me

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -12,9 +12,9 @@ export const Projects = () => {
             <h6 className="text-3xl md:text-6xl text-gray-200 uppercase tracking-[8px] mb-32">Projects</h6>
             
             <div className="flex justify-around gap-12">
-                <ProjectCard title="Crispy-barnacle" description="My role was developing api and websockets to register, login and send messages." devStack={["Golang", "PostgreSQL","jwt"]} repo="https://github.com/szymon676/crispy-barnacle" />
-                <ProjectCard title="Go-nimbus" description="Golang web framework that provides flexibility." devStack={["Golang"]} repo="https://github.com/szymon676/Go-nimbus" />
-                <ProjectCard title="Finance Tracker" description="api to keep track of your funds." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/finance-tracker" />
+                <ProjectCard title="Crispy-barnacle" description="Chat aplication with chat rooms." devStack={["Golang", "PostgreSQL","sockets","auth system"]} repo="https://github.com/szymon676/crispy-barnacle" />
+                <ProjectCard title="Restaurant Managment" description="Restaurant managment microservices project." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/restaurant-managment" />
+                <ProjectCard title="Finance Tracker" description="Monolith api to keep track of your funds." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/finance-tracker" />
             </div>
         </motion.div>
     )
