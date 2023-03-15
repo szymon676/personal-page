bd5aea1a5e3b39ba28165ffca1c47377e1deb8d9
Szymon <sgil73072@gmail.com>
2023-03-15 07:52:51 +0100

    change text

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -12,9 +12,9 @@ export const Projects = () => {
             <h6 className="text-3xl md:text-6xl text-gray-200 uppercase tracking-[8px] mb-32">Projects</h6>
             
             <div className="flex justify-around gap-12">
-                <ProjectCard title="Crispy-barnacle" description="Chat aplication with chat rooms." devStack={["Golang", "PostgreSQL","sockets","auth system"]} repo="https://github.com/szymon676/crispy-barnacle" />
+                <ProjectCard title="Crispy-barnacle" description="Aplication to communicate with your friend via chat rooms." devStack={["Golang", "PostgreSQL","sockets"]} repo="https://github.com/szymon676/crispy-barnacle" />
                 <ProjectCard title="Restaurant Managment" description="Restaurant managment microservices project." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/restaurant-managment" />
-                <ProjectCard title="Finance Tracker" description="Monolith api to keep track of your funds." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/finance-tracker" />
+                <ProjectCard title="Jobguru" description="Jobguru is a web application that allows employers to post job openings and job seekers to search for and apply to jobs." devStack={["Golang","PostgreSQL"]} repo="https://github.com/szymon676/finance-tracker" />
             </div>
         </motion.div>
     )
